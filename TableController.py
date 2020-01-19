f9af17fa87d4c63423ff8a02d1adcb1a5ca8fdf3
KonDob <konstantin.dobro@gmail.com>
2020-01-19 18:58:43 +0200

    Upgrading

--- TableController.py
+++ TableController.py
@@ -1,34 +1,46 @@
 import sqlite3
 
 
-class Table:
+class Table():
     """
     Class to manipulate with sql tables
     """
-    connection = None
 
-def connect_to_db():
-    """
-    Method to create and connect to DB
-    If DB already exists it just connect to it.
-    """
     connection = None
-    try:
-        connection = sqlite3.connect('test_database.db')
-        return connection
-    except:
-        print(Error)
+    cursor = None
 
-def create_table():
-    """
-    Create a table
-    """
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS result_files (
+    def connect_to_db(self):
+        """
+        Method to create and connect to DB
+        If DB already exists it just connect to it.
+        """
+        try:
+            self.connection = sqlite3.connect('test_database.db')
+            self.cursor = sqlite3.connect('test_database.db').cursor()
+            return self.connection, self.cursor
+        except EnvironmentError:
+            print(EnvironmentError)
+
+
+    def create_table(self,name):
+        """
+        Create a table
+        """
+        # self.cursor = self.connection.cursor()
+        self.cursor.execute(""" CREATE TABLE IF NOT EXISTS {} (
                                         id integer PRIMARY KEY,
                                         path_to_source text NOT NULL,
                                         path_to_result_file text NOT NULL,
                                         size_of_result_file text NOT NULL
-                                    ); """)
+                                    ); """.format(name))
+    
+    def write_data_to_table(self, table_name, *args):
+        self.cursor.execute(
+            """
+            INSERT INTO {}({})
+            VALUES()
+            
+            """.format(table_name,*args)
+        )
 
-connection = connect_to_db()
-cursor = connection.cursor()
+    
---
+++ sweet_script.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os
+import sys
+import sqlite3
+from CSV_Agent import CSVAgent
+import Folder_controller
+from TableController import Table
+
+PATH_TO_CVS = os.path.join('Default_data_for_test_task/data.csv')
+csv = CSVAgent(PATH_TO_CVS)
+fc = Folder_controller.Folder()
+db = Table()
+
+#Create DB
+db.connect_to_db()
+
+#Create Table result_files()
+db.create_table('result_files')
+db.write_data_to_table('result_files','path')
+# Find record id data.csv by ID
+# fc.create_folder('result_data')
+# result_data = csv.find_record_by_id()
+
+# Copiyng ID folder with content to parent folder
+# fc.copy_folder(result_data)
